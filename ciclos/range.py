50066e253dbb641be593d666970a5059c74ed916
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 15:41:21 -0600

    cambios range.py

--- ciclos/range.py
+++ ciclos/range.py
@@ -1,2 +1,3 @@
-for valor in range (3, 11):
-    print(valor)
+#El rango va del 3 al 10, pero va saltando de 2 en 2
+for valor in range (3, 11, 2): # el numero que indica el stop es excluyente
+    print(valor) #se imprimen en pantalla 3,5,7,9
