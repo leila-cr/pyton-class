714cd90a8aa55b99b3119a10c7298de436506fc0
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 17:25:24 -0600

    cambios string.py

--- ciclos/string.py
+++ ciclos/string.py
@@ -1,3 +1,3 @@
-secuencia = " CCGTAAGTC"
-for nucleotido in secuencia:
+secuencia = "CCGTAAGTC" #string objetos iterables
+for nucleotido in secuencia: #itera sobre cada caracter sin necesidad de dar la posicion
     print(nucleotido)
