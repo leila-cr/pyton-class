b24fa32c1177813cd32ac51bce05d63f41586cd8
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 15:04:08 -0600

    cambios genes.py

--- ciclos/genes.py
+++ ciclos/genes.py
@@ -1,4 +1,4 @@
 genes = ["crp", "recA", "lacZ", "leuD"] #lista strings
 
-for gen in genes: # ciclo que itera sobre la lista genes
+for gen in genes [::-1]: # ciclo que itera sobre la lista genes
     print(f"El nombre del gen es {gen}")
