3c4d39ba662c9d0be39dc96e2dfedaa49371a78b
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 18:18:28 -0600

    cambios identaciones.py

--- ciclos/identaciones.py
+++ ciclos/identaciones.py
@@ -1,7 +1,14 @@
-apes = ["Pongo py", "Pan nosequ", "Gor noseque"]
+apes = ["Pongo pygmaeus", "Pan njfifkso", "Gor noseque"]
 
 for ape in apes:
-    name_length = len(ape)
-    first_letter = ape[0]
-    print (f"{ape} is an ape. Its name starts with {first_letter}") #entre las llaves van las variables igual se pueden hacer operaciones dentro
-    print (f"Its name has {len(ape)}")  # es por que se esta concatenando un string con un int
+    print (f"{ape} is an ape. Its name starts with {ape[0]}") # en este caso se asignan las variables en el print
+    print (f"Its name has {len(ape)}")  
+
+
+    #name_length = len(ape)
+    #first_letter = ape[0]
+    #print (f"{ape} is an ape. Its name starts with {first_letter}") #variable dentro de la llave. tambien se pueden operaciones
+    #print (f"Its name has {name_length}")  
+
+    # print (ape + " is an ape. Its name starts with" + first_letter)
+    # print ("Its name has" + str (name_length))  Funcion str se esta concatenando un string con un int
