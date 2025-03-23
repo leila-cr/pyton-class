d8978f8736f5daa5ce248192f97766e9424c76a0
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 19:43:04 -0600

    cambios suma.py

--- ciclos/suma.py
+++ ciclos/suma.py
@@ -1,9 +1,15 @@
-numeros_str = input ("dame 3 numeros separados por espacios").split() #al aplicar lo convierte en una lista al principio era un string 
-lista_numeros = (list(map(int,numeros_str)))
+numeros_str = input ("Dame 3 numeros separados por espacios").split() # se convierte numeros ingresados en lista de strings  
+lista_numeros = (list(map(int,numeros_str))) #funcion map convierte los elementos de la lista en enteros
 
 
-#print(numeros_str)
-suma = sum(lista_numeros) #sum trabaja con iterables y map es un iterable, por lo tanto en este problema no es tan necesario agregar el list
+suma = sum(lista_numeros) #sum trabaja con iterables y map es iterable, por lo tanto en este problema no es tan necesario agregar el list
 print(suma)
 
- 
+#numeros_str = input ("Dame 3 numeros separados por espacios").split() # se convierte numeros ingresados en lista de strings  
+#lista_numeros = map(int,numeros_str) #el objeto map tambien es un iterable
+
+
+#suma = sum(lista_numeros) 
+#print(suma)
+
+
