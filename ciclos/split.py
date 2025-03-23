db247e2146aa02d2d0d85a0c97a9f07716da3856
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 19:03:45 -0600

    cambios split.py

--- ciclos/split.py
+++ ciclos/split.py
@@ -1,13 +1,21 @@
-texto = "ATG CCT TAA GGT"
+texto = "ATG CCT TAA GGT" #string
 lista = texto.split()
-print(lista)
+print(lista) #se imprime una lista en la que el delimitador es automaticamente el espacio
 
 texto = "ATG,CCT,TAA,GGT"
-lista = texto.split(",") #puede se con comas u apostrofes pues es un string
+lista = texto.split(",") #comas son el delimitador 
 print(lista)
 
 texto = "ATG-CCT-TAA-GGT"
-lista = texto.split("-", 2) #solo son normales los primero sque tienen el -, ya despues lo imprime todo lo que sigue
+lista = texto.split("-", 2) #guiones son el delimitador, se toman en cuenta unicamente los dos primeros 
 print(lista)
 
-#input recive strings y sum numeros
+texto = "ATGCCTTAAGGT"
+lista = texto.split("T", 2) #solo son normales los primero sque tienen el -, ya despues lo imprime todo lo que sigue
+print(lista)
+
+texto = "ATG\tCCT\tTAA\tGGT" #tabuladores en un string
+lista = texto.split("\t") #el delimitador sera el string
+print(lista)
+
+
