471ce0259d496adfbc0206ca0373a0389b1fb220
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 21:28:00 -0600

    cambios funciones.py

--- ciclos/funciones.py
+++ ciclos/funciones.py
@@ -1,7 +1,7 @@
-#caso de funcion enumerate
+#funcion enumerate
 secuencia = "GCCTAAGGC"
 
-for i, base in enumerate(secuencia):
+for i, base in enumerate(secuencia): #enumerate da el indice de la posicion del string, ademas de la letra que lo ocupa
     print(f"Posicion {i} : {base}")
 
 
@@ -9,9 +9,12 @@ for i, base in enumerate(secuencia):
 bases = "ATGC"
 complementos = "TACG"
 
-for base,complemento in zip(bases, complementos): #con zip se pueden iterar mas de 1 elemento
+for base,complemento in zip(bases, complementos): #zip permite iterar con 2 elementos
     print (f"{base}---> {complemento}")
 
-
+#otro caso con la funcion zip
 genes = ["AraC", "LE8", "THR8"]
-longitudes = [356, 89] #... terminarlo
+longitudes = [356, 89] 
+
+for gen, long in zip(genes, longitudes):#en este caso zip ignorara los elementos sobrantes
+    print(f"Gen {gen} ----> {long}pb")
