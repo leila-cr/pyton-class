8d780f54247da22b0ac7065164f6ff81f01c7e5b
leila-cr <leilanic@lcg.unam.mx>
2025-03-22 20:38:12 -0600

    cambios tamano.py

--- ciclos/tamano.py
+++ ciclos/tamano.py
@@ -1,8 +1,13 @@
 with open ("genes.gff") as file:
-    for linea in file:
-        print(linea)
-        columnas  = linea.strip().split("\t")
-        tamano = int(columnas[4])-int(columnas[3])+1
+    for linea in file: 
+        columnas  = linea.strip().split("\t") #se eliminan espacios de lineas, posteriormente se convierte en una lista strings delimitada por \t 
+        tamano = int(columnas[4])-int(columnas[3])+1 #se cambia a lista de enteros y se obtiene resta de la columna 4 y 3 
+        print(tamano)
 
-   #tengo que hacer aui lo de archivo nano y aque lo hice en otro lado
-        
+
+   
+#with open ("genes.gff") as file:
+    #for linea in file:#itera sobre cada linea en el archivo
+        #print(linea)
+        #columnas  = linea.strip().split("\t") #quitar espacios y con el split delimitar 
+        #print(columnas[3])# se imprime la columna 3 pero no como lista checar eso
